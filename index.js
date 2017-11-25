42869b6a6440cf74aa69e8f64d86873d4f02f665
Luis Carlos Mencia <lcmencia2@gmail.com>
2017-11-25 20:51:26 -0300

    Create an endpoint with GET and parameters in your API REST

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const port = process.env.PORT || 3001
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
 
+app.get('/hola/:name',(req, res) => {
+    res.send({ message: `Hola ${req.params.name}!`})
+})
+
 app.listen(port, () => {
     console.log(`API REST running in http://localhost:${port}`)
 })
