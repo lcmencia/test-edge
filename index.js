063c60290a04d0f80270dbb0fe9b606a5c0cca26
Luis Carlos Mencia <lcmencia2@gmail.com>
2017-11-25 21:32:10 -0300

    Testing POSTMAN

--- index.js
+++ index.js
@@ -9,9 +9,27 @@ const port = process.env.PORT || 3001
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
 
-app.get('/hola/:name',(req, res) => {
-    res.send({ message: `Hola ${req.params.name}!`})
+app.get('/api/product', (req, res) => {
+    res.send(200, {products: []})
 })
+
+app.get('/api/product/:product', (req, res) => {
+    
+})
+
+app.post('/api/product', (req, res) => {
+    console.log(req.body)
+    res.status(404).send({message:'El producto no existe'})
+})
+
+app.put('/api/product/:productId', (req, res) => {
+    
+})
+
+app.delete('/api/product/:productId', (req, res) => {
+    
+})
+
 
 app.listen(port, () => {
     console.log(`API REST running in http://localhost:${port}`)
