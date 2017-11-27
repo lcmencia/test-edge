1506ba99e2eab7bf31123347cbb83bfa31e48531
Luis Carlos Mencia <lcmencia2@gmail.com>
2017-11-27 19:35:19 -0300

    Login and registration of users with Token in your REST API

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,8 +1,6 @@
 'use strict'
 
-const jwt = require('jwt')
-const moment = require('moment')
-const config = require('../config')
+const services = require('../services')
 
 function isAuth (req, res, next){
     if (!req.headers.authorization){
@@ -10,14 +8,17 @@ function isAuth (req, res, next){
     }
 
     const token = req.headers.authorization.split(" ")[1]
-    const payload = jwt.decode(token, config.SECRET_TOKEN)
+    
+    services.decodedToken(token)
+        .then(response => {
+            req.user = response
+            next()
+        })
+        .catch(response => {
+            res.status(response.status)
+        })
 
-    if (payload.exp <= moment().unix()) {
-        return res.status(401).send({ message: 'El Token ha expirado' })
-    }
-
-    req.user = payload.sub
-    next()
+    
 }
 
 module.exports = isAuth
