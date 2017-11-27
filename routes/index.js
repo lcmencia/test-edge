3588007dab319ed496a002bc2d4ee66ed4d99663
Luis Carlos Mencia <lcmencia2@gmail.com>
2017-11-27 17:10:21 -0300

    Protect routes in your REST API with JSON Web Tokens

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 
 const express = require('express')
 const productCtrl = require('../controllers/product')
+const auth = require('../middlewares/auth')
 const api = express.Router()
 
 api.get('/product', productCtrl.getProducts)
@@ -9,5 +10,8 @@ api.get('/product/:productId', productCtrl.getProduct)
 api.post('/product', productCtrl.saveProduct)
 api.put('/product/:productId', productCtrl.updateProduct)
 api.delete('/product/:productId', productCtrl.deleteProduct)
+api.get('/private', auth.isAuth, function(req, res){
+    res.status(200).send({ message: 'Tienes acceso' })
+})
 
 module.exports = api
